3fe055a8face742a1ecab838266b5260581f84fc
VEEETOOORRR <antonni2601.ag@gmail.com>
2026-07-19 02:08:42 -0300

    componente display funcionando

---
+++ components/display/include/display.h
@@ -0,0 +1,30 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include "driver/spi_master.h"
+#include "string.h"
+#include <stdio.h>
+
+#define PIN_NUM_MISO   13   // O MAX7219 não envia dados de volta, pino não usado
+#define PIN_NUM_MOSI   11   // Conecta no DIN da primeira matriz
+#define PIN_NUM_CLK    12   // Conecta no CLK das matrizes
+#define PIN_NUM_CS     10   // Conecta no CS/LOAD das matrizes
+
+#define NUM_MODULES    8    // Quantidade de matrizes 8x8 encadeadas
+
+// Registradores do MAX7219
+#define MAX7219_REG_NOOP         0x00
+#define MAX7219_REG_DIGIT0       0x01 // Linhas/Colunas de 0 a 7
+#define MAX7219_REG_DECODE_MODE  0x09
+#define MAX7219_REG_INTENSITY    0x0A
+#define MAX7219_REG_SCAN_LIMIT   0x0B
+#define MAX7219_REG_SHUTDOWN     0x0C
+#define MAX7219_REG_DISPLAY_TEST 0x0F
+
+#define DISPLAY_INTENSITY        0x01
+
+void spi_conf();
+
+void max7219_write_all(uint8_t reg, uint8_t data);
+
+#endif
---
+++ main/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "driver/ledc.h"
+#include "esp_adc/adc_oneshot.h"
+#include "esp_timer.h"
+#include "display.h"
+
+void app_main(){
+
+    spi_conf();
+
+    // Pequeno loop de teste: Pisca o primeiro LED de cada matriz para testar
+    while(1) {
+        max7219_write_all(MAX7219_REG_DIGIT0, 0x01); // Liga o primeiro bit da linha 0
+        vTaskDelay(pdMS_TO_TICKS(500));
+        max7219_write_all(MAX7219_REG_DIGIT0, 0x00); // Desliga
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+
+}
+
