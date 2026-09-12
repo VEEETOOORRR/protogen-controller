4c6a5148ae9e066966079864d3db86573dfed023
VEEETOOORRR <antonni2601.ag@gmail.com>
2026-09-12 17:44:23 -0300

    encapsulamento de controle de display em task separada

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -1,48 +1,14 @@
 #include <stdio.h>
-#include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/gpio.h"
-#include "driver/ledc.h"
-#include "esp_adc/adc_oneshot.h"
-#include "esp_timer.h"
+
 #include "display.h"
+#include "boopsensor.h"
 
-static StaticFace_t buffer;
+void app_main(void) {
+    // Inicializa o subsistema de display 
+    display_init();
 
-void teste(){
-    for(int i = 0; i < 8; i++){
-        display_write_all(MAX7219_REG_DIGIT0 + i, 1 << i);
-    }
-
-    vTaskDelay(100);
-
-    for(int i = 0; i < 8; i++)
-    display_write_all(MAX7219_REG_DIGIT0 + i, 0);
-
-    vTaskDelay(100);
-
+    // Inicializa o sensor de boop
+    boopsensor_init();
 }
-
-
-void app_main(){
-
-    spi_conf();
-    display_clean_all();
-
-    buffer_update_left_eye(&buffer, EXPRESSAO_OLHOESQUERDO);
-    buffer_update_left_mouth(&buffer, EXPRESSAO_BOCAESQUERDA);
-    buffer_update_left_nose(&buffer, EXPRESSAO_NARIZESQUERDO);
-    buffer_update_right_nose(&buffer, EXPRESSAO_NARIZDIREITO);
-    buffer_update_right_mouth(&buffer, EXPRESSAO_BOCADIREITA);
-    buffer_update_right_eye(&buffer, EXPRESSAO_OLHODIREITO);
-
-
-
-    while(1) {
-        display_update(&buffer);
-        vTaskDelay(100);
-
-    }
-}
-
