73dec415f7dffcdf40119d48e847704bfb88f61d
VEEETOOORRR <antonni2601.ag@gmail.com>
2026-09-15 14:48:44 -0300

    Ativação da ventoinha por padrão

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -4,6 +4,7 @@
 
 #include "display.h"
 #include "boopsensor.h"
+#include "fan.h"
 
 void app_main(void) {
     // Inicializa o subsistema de display 
@@ -11,4 +12,8 @@ void app_main(void) {
 
     // Inicializa o sensor de boop
     boopsensor_init();
+
+    pwm_init();
+    set_duty(100);
+    
 }
